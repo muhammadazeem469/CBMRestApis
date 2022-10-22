c47f5e32fa2b50406171f175ce982012254fea2c
unknown <muhammadazeem469@gmail.com.com>
2022-10-22 12:50:50 -0400

    founderupdate

--- source/model/founder.js
+++ source/model/founder.js
@@ -1,7 +1,7 @@
 const dbConnection = require('../Database/config')
 
 const CreateFoundertable = (req, res) => {
-    let sql = `DROP TABLE IF EXISTS founders; CREATE TABLE founders(id int AUTO_INCREMENT, FullName VARCHAR(255), Title VARCHAR(255), company_id int not null,foreign key (company_id) references  companies(id), PRIMARY KEY(id))`;    
+    let sql = `DROP TABLE IF EXISTS founders; CREATE TABLE founders(id int AUTO_INCREMENT, FullName VARCHAR(255), Title VARCHAR(255), company_id int not null,foreign key (company_id) references  companies(id) ON DELETE CASCADE, PRIMARY KEY(id))`;    
     dbConnection.query(sql, (err, result) => {
         if(err){
             console.log(err)
